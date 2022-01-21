79a2fa63d040f58c9a26d8a65c7fa0545d08fba4
Postmodern <postmodern.mod3@gmail.com>
2022-01-20 23:15:14 -0800

    Changed the license from GPL-3.0 to LGPL-3.0.

--- lib/ronin/post_ex/resources.rb
+++ lib/ronin/post_ex/resources.rb
@@ -6,17 +6,17 @@
 # This file is part of ronin-post_ex.
 #
 # ronin-post_ex is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
 # ronin-post_ex is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# GNU Lesser General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with ronin-post_ex.  If not, see <https://www.gnu.org/licenses/>
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-post_ex.  If not, see <https://www.gnu.org/licenses/>.
 #
 
 require 'open_namespace'
