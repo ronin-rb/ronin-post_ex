d58332ede8bc370f75760b723df681830a15adf1
Postmodern <postmodern.mod3@gmail.com>
2022-11-13 08:54:37 -0800

    Added a `Sessions::Session` base class (closes #32).

---
+++ lib/ronin/post_ex/sessions/session.rb
@@ -0,0 +1,40 @@
+#
+# ronin-post_ex - a Ruby API for Post-Exploitation.
+#
+# Copyright (c) 2007-2022 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# ronin-post_ex is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-post_ex is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-post_ex.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+module Ronin
+  module PostEx
+    module Sessions
+      class Session
+
+        #
+        # The session name.
+        #
+        # @return [String]
+        #
+        # @raise [NotImplementedError]
+        #   The session class did not set `@name`.
+        #
+        def name
+          @name || raise(NotImplementedError,"#{self.class}#name was not set")
+        end
+
+      end
+    end
+  end
+end
---
+++ spec/sessions/session_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+require 'ronin/post_ex/sessions/session'
+
+describe Ronin::PostEx::Sessions::Session do
+  describe "#name" do
+    context "when #name is set" do
+      module TestSession
+        class SessionWithNameSet < Ronin::PostEx::Sessions::Session
+
+          def initialize(name)
+            @name = name
+          end
+
+        end
+      end
+
+      let(:name) { 'example-session' }
+
+      subject { TestSession::SessionWithNameSet.new(name) }
+
+      it "must return @name" do
+        expect(subject.name).to eq(name)
+      end
+    end
+
+    context "when @name is not set" do
+      module TestSession
+        class SessionWithoutNameSet < Ronin::PostEx::Sessions::Session
+        end
+      end
+
+      subject { TestSession::SessionWithoutNameSet.new }
+
+      it do
+        expect {
+          subject.name
+        }.to raise_error(NotImplementedError,"#{subject.class}#name was not set")
+      end
+    end
+  end
+end
