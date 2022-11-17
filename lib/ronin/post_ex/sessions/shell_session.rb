bd9fde00ff9e2d83c1a6294ed08aeb22164e1a52
Postmodern <postmodern.mod3@gmail.com>
2022-11-17 08:45:21 -0800

    Emulate most of the post-exploitation API in `ShellSession` with shell commands.
    * Added `ShellSession#shell_gets`.
    * Added `ShellSession#shell_puts`.
    * Added `ShellSession#command_join`.
    * Added `ShellSession#command_exec`.

--- lib/ronin/post_ex/sessions/shell_session.rb
+++ lib/ronin/post_ex/sessions/shell_session.rb
@@ -19,6 +19,7 @@
 
 require 'ronin/post_ex/sessions/session'
 
+require 'shellwords'
 require 'base64'
 
 module Ronin
@@ -26,6 +27,11 @@ module Ronin
     module Sessions
       #
       # Base class for all interactive shell based post-exploitation sessions.
+      #
+      # ## Features
+      #
+      # * Supports Bash, Zsh, and all POSIX shells.
+      # * Emulates most of the post-exploitation API via shell commands.
       #
       class ShellSession < Session
 
@@ -46,6 +52,33 @@ module Ronin
           @io = io
         end
 
+        #
+        # @group Shell Methods
+        #
+
+        #
+        # Writes a line to the shell.
+        #
+        # @param [String] line
+        #   The line to write.
+        #
+        # @api private
+        #
+        def shell_puts(line)
+          @io.write("#{line}\n")
+        end
+
+        #
+        # Reads a line from the shell.
+        #
+        # @return [String, nil]
+        #
+        # @api private
+        #
+        def shell_gets
+          @io.gets
+        end
+
         # Deliminator line to indicate the beginning and end of output
         DELIMINATOR = '---'
 
@@ -59,10 +92,10 @@ module Ronin
         #   The output of the shell command.
         #
         def shell_exec(command)
-          @io.write("echo #{DELIMINATOR}; #{command} 2>/dev/null | base64; echo #{DELIMINATOR}\n")
+          shell_puts("echo #{DELIMINATOR}; #{command} 2>/dev/null | base64; echo #{DELIMINATOR}")
 
           # consume any leading output before the command output
-          while (line = @io.gets)
+          while (line = shell_gets)
             if line.chomp == DELIMINATOR
               break
             end
@@ -70,7 +103,7 @@ module Ronin
 
           output = String.new
 
-          while (line = @io.gets)
+          while (line = shell_gets)
             if line.chomp == DELIMINATOR
               break
             end
@@ -79,6 +112,485 @@ module Ronin
           end
 
           return Base64.decode64(output)
+        end
+
+        #
+        # Joins a command with arguments into a single command String.
+        #
+        # @param [String] command
+        #   The command name to execute.
+        #
+        # @param [Array<String>] arguments
+        #   Additional arguments for the command.
+        #
+        # @return [String]
+        #   The command string.
+        #
+        # @api private
+        #
+        def command_join(command,*arguments)
+          Shellwords.join([command,*arguments])
+        end
+
+        #
+        # Invokes a specific command with arguments.
+        #
+        # @param [String] command
+        #   The command name to execute.
+        #
+        # @param [Array<String>] arguments
+        #   Additional arguments for the command.
+        #
+        # @return [String, nil]
+        #   The command's output or `nil` if there was no output.
+        #
+        # @api private
+        #
+        def command_exec(command,*arguments)
+          output = shell_exec(command_join(command,*arguments))
+
+          if output.empty? then nil
+          else                  output
+          end
+        end
+
+        #
+        # @group System Methods
+        #
+
+        #
+        # Gets the current time and returns the UNIX timestamp.
+        #
+        # @return [Integer]
+        #   The current time as a UNIX timestamp.
+        #
+        # @note executes the `date +%s` command.
+        #
+        def sys_time
+          shell_exec('date +%s').to_i
+        end
+
+        #
+        # @group File-System methods
+        #
+
+        #
+        # Gets the current working directory and returns the directory path.
+        #
+        # @return [String]
+        #   The remote current working directory.
+        #
+        # @note executes the `pwd` command.
+        #
+        def fs_getcwd
+          shell_exec('pwd').chomp
+        end
+
+        #
+        # Changes the current working directory.
+        #
+        # @param [String] path
+        #   The new remote current working directory.
+        #
+        # @note executes the `cd <path>` command.
+        #
+        def fs_chdir(path)
+          shell_puts("cd #{Shellwords.escape(path)} 2>/dev/null")
+        end
+
+        #
+        # Reads the entire file at the given path and returns the full file's
+        # contents.
+        #
+        # @param [String] path
+        #   The remote path to read.
+        #
+        # @return [String, nil]
+        #   The contents of the remote file or `nil` if the file could not be
+        #   read.
+        #
+        # @note executes the `cat <path>` command.
+        #
+        def fs_readfile(path)
+          command_exec('cat',path)
+        end
+
+        #
+        # Reads the destination path of a remote symbolic link.
+        #
+        # @param [String] path
+        #   The remote path to read.
+        #
+        # @return [String, nil]
+        #   The destination of the remote symbolic link or `nil` if the symbolic
+        #   link could not be read.
+        #
+        # @note executes the `readlink -f <path>` command.
+        #
+        def fs_readlink(path)
+          command_exec('readlink','-f',path).chomp
+        end
+
+        #
+        # Reads the contents of a remote directory and returns an Array of
+        # directory entry names.
+        #
+        # @param [String] path
+        #   The path of the remote directory to read.
+        #
+        # @return [Array<String>]
+        #   The entities within the remote directory.
+        #
+        # @note executes the `ls <path>` command.
+        #
+        def fs_readdir(path)
+          command_exec('ls',path).lines(chomp: true)
+        end
+
+        #
+        # Evaluates a directory glob pattern and returns all matching paths.
+        #
+        # @param [String] pattern
+        #   The glob pattern to search for remotely.
+        #
+        # @return [Array<String>]
+        #   The matching paths.
+        #
+        # @note executes the `ls <pattern>` command.
+        #
+        def fs_glob(pattern,&block)
+          shell_exec("ls #{pattern}").lines(chomp: true)
+        end
+
+        #
+        # Creates a remote temporary file with the given file basename.
+        #
+        # @param [String] basename
+        #   The basename for the new temporary file.
+        #
+        # @return [String]
+        #   The path of the newly created temporary file.
+        #
+        # @note executes the `mktemp <basename>` command.
+        #
+        def fs_mktemp(basename)
+          command_exec('mktemp',basename).chomp
+        end
+
+        #
+        # Creates a new remote directory at the given path.
+        #
+        # @param [String] new_path
+        #   The new remote directory to create.
+        #
+        # @note executes the `mkdir <path>` command.
+        #
+        def fs_mkdir(path)
+          command_exec('mkdir',path)
+        end
+
+        #
+        # Copies a source file to the destination path.
+        #
+        # @param [String] src
+        #   The source file.
+        #
+        # @param [String] dest
+        #   The destination path.
+        #
+        # @note executes the `cp -r <path> <new_path>` command.
+        #
+        def fs_copy(path,new_path)
+          command_exec('cp','-r',path,new_path)
+        end
+
+        #
+        # Removes a file at the given path.
+        #
+        # @param [String] path
+        #   The remote path to remove.
+        #
+        # @note executes the `rm <path>` command.
+        #
+        def fs_unlink(path)
+          command_exec('rm',path)
+        end
+
+        #
+        # Removes an empty directory at the given path.
+        #
+        # @param [String] path
+        #   The remote directory path to remove.
+        #
+        # @note executes the `rmdir <path>` command.
+        #
+        def fs_rmdir(path)
+          command_exec('rmdir',path)
+        end
+
+        #
+        # Moves or renames a remote source file to a new destination path.
+        #
+        # @param [String] src
+        #   The source file path.
+        #
+        # @param [String] dest
+        #   The destination file path.
+        #
+        # @note executes the `mv <path> <new_path>` command.
+        #
+        def fs_move(path,new_path)
+          command_exec('mv',path,new_path)
+        end
+
+        #
+        # Creates a remote symbolic link at the destination path pointing to the
+        # source path.
+        #
+        # @param [String] src
+        #   The source file path for the new symbolic link.
+        #
+        # @param [String] dest
+        #   The remote path of the new symbolic link.
+        #
+        # @note executes the `ln -s <src> <dest>` command.
+        #
+        def fs_link(src,dest)
+          command_exec('ln','-s',src,dest)
+        end
+
+        #
+        # Changes the group ownership of a remote file or directory.
+        #
+        # @param [String] group
+        #   The new group name for the remote file or directory.
+        #
+        # @param [String] path
+        #   The path of the remote file or directory.
+        #
+        # @note executes the `chgrp <group> <path>` command.
+        #
+        def fs_chgrp(group,path)
+          command_exec('chgrp',group,path)
+        end
+
+        #
+        # Changes the user ownership of remote a file or directory.
+        #
+        # @param [String] user
+        #   The new user for the remote file or directory.
+        #
+        # @param [String] path
+        #   The path of the remote file or directory.
+        #
+        # @note executes the `chown <user> <path>` command.
+        #
+        def fs_chown(user,path)
+          command_exec('chown',user,path)
+        end
+
+        #
+        # Changes the permissions on a remote file or directory.
+        #
+        # @param [Integer] mode
+        #   The permissions mode for the remote file or directory.
+        #
+        # @param [String] path
+        #   The path of the remote file or directory.
+        #
+        # @note executes the `chmod <umask> <path>` command.
+        #
+        def fs_chmod(mode,path)
+          umask = "%.4o" % mode
+
+          command_exec('chmod',umask,path)
+        end
+
+        #
+        # Queries file information for the given remote path and returns a Hash
+        # of file metadata.
+        #
+        # @param [String] path
+        #   The path to the remote file or directory.
+        #
+        # @return [Hash{Symbol => Object}, nil]
+        #   The metadata for the remote file.
+        #
+        # @note executes the `stat -t <path>` command.
+        #
+        def fs_stat(path)
+          fields = command_exec('stat','-t',path).strip.split(' ')
+
+          return {
+            path:      path,
+            size:      fields[1].to_i,
+            blocks:    fields[2].to_i,
+            uid:       fields[4].to_i,
+            gid:       fields[5].to_i,
+            inode:     fields[7].to_i,
+            links:     fields[8].to_i,
+            atime:     Time.at(fields[11].to_i),
+            mtime:     Time.at(fields[12].to_i),
+            ctime:     Time.at(fields[13].to_i),
+            blocksize: fields[14].to_i
+          }
+        end
+
+        #
+        # @group Process methods
+        #
+
+        #
+        # Gets the current process's Process ID (PID).
+        #
+        # @return [Integer]
+        #   The current process's PID.
+        #
+        # @note executes the `echo $$` command.
+        #
+        def process_getpid
+          shell_exec('echo $$').to_i
+        end
+
+        #
+        # Gets the current process's parent Process ID (PPID).
+        #
+        # @return [Integer]
+        #   The current process's PPID.
+        #
+        # @note executes the `echo $PPID` command.
+        #
+        def process_getppid
+          shell_exec('echo $PPID').to_i
+        end
+
+        #
+        # Gets the current process's user ID (UID).
+        #
+        # @return [Integer]
+        #   The current process's UID.
+        #
+        # @note executes the `id -u` command.
+        #
+        def process_getuid
+          command_exec('id','-u').to_i
+        end
+
+        #
+        # Gets the current process's group ID (GID).
+        #
+        # @return [Integer]
+        #   The group ID (GID) for the current process.
+        #
+        # @note executes the `id -g` command.
+        #
+        def process_getgid
+          command_exec('id','-g').to_i
+        end
+
+        #
+        # Queries all environment variables of the current process. Returns a
+        # Hash of the env variable names and values.
+        #
+        # @return [Hash{String => String}]
+        #   The Hash of environment variables.
+        #
+        # @note executes the `env` command.
+        #
+        def process_environ
+          Hash[command_exec('env').each_line(chomp: true).map { |line|
+            line.split('=',2)
+          }]
+        end
+
+        #
+        # Gets an individual environment variable. If the environment variable
+        # has not been set, `nil` will be returned.
+        #
+        # @param [String] name
+        #   The environment variable name to get.
+        #
+        # @return [String]
+        #   The environment variable value.
+        #
+        # @note executes the `echo $<name>` command.
+        #
+        def process_getenv(name)
+          shell_exec("echo $#{name}").chomp
+        end
+
+        #
+        # Sets an environment variable to the given value.
+        #
+        # @param [String] name
+        #   The environment variable name to set.
+        #
+        # @param [String] value
+        #   The new value for the environment variable.
+        #
+        # @note executes the `export <name>=<value>` command.
+        #
+        def process_setenv(name,value)
+          shell_puts("export #{name}=#{value}")
+        end
+
+        #
+        # Un-sets an environment variable.
+        #
+        # @param [String] name
+        #   The environment variable to unset.
+        #
+        # @note executes the `unset <name>` command.
+        #
+        def process_unsetenv(name)
+          shell_puts("unset #{name}")
+        end
+
+        #
+        # Kills another process using the given Process ID (POD) and the signal
+        # number.
+        #
+        # @param [Integer] pid
+        #   The process ID (PID) to kill.
+        #
+        # @param [Integer] signal
+        #   The signal to send the process ID (PID).
+        #
+        # @note executes the `kill -s <signal> <pid>` command.
+        #
+        def process_kill(pid,signal)
+          command_exec('kill','-s',signal,pid)
+        end
+
+        #
+        # Spawns a new process using the given program and additional arguments.
+        #
+        # @param [String] command
+        #   The command name to spawn.
+        #
+        # @param [Array<String>] arguments
+        #   Additional arguments for the program.
+        #
+        # @return [Integer]
+        #   The process ID (PID) of the spawned process.
+        #
+        # @note
+        #   executes the command with additional arguments as a background
+        #   process.
+        #
+        def process_spawn(command,*arguments)
+          command = command_join(command,*arguments)
+
+          shell_exec("#{command} 2>&1 >/dev/null &; echo $!").to_i
+        end
+
+        #
+        # Exits the current process.
+        #
+        # @note executes the `exit` command.
+        #
+        def process_exit
+          shell_puts('exit')
         end
 
         #
--- spec/sessions/shell_session_spec.rb
+++ spec/sessions/shell_session_spec.rb
@@ -16,6 +16,27 @@ describe Ronin::PostEx::Sessions::ShellSession do
     end
   end
 
+  describe "#shell_gets" do
+    it "must read a line of text from #io" do
+      thread = Thread.new do
+        sleep 0.1
+        shell.write("foo bar\n")
+      end
+
+      expect(subject.shell_gets).to eq("foo bar\n")
+    end
+  end
+
+  describe "#shell_puts" do
+    let(:line) { "foo bar" }
+
+    it "must write a line of text to #io" do
+      subject.shell_puts(line)
+
+      expect(shell.gets).to eq("#{line}\n")
+    end
+  end
+
   describe "#shell_exec" do
     let(:command) { 'ls' }
     let(:output) do
@@ -58,6 +79,385 @@ describe Ronin::PostEx::Sessions::ShellSession do
       end
 
       expect(subject.shell_exec(command)).to eq(output)
+    end
+  end
+
+  describe "#sys_time" do
+    let(:timestamp) { 1668692783         }
+    let(:output)    { "#{timestamp}\n"   }
+
+    it "must run the 'date +%s' and parse the output as an Integer" do
+      expect(subject).to receive(:shell_exec).with('date +%s').and_return(output)
+
+      expect(subject.sys_time).to eq(timestamp)
+    end
+  end
+
+  describe "#fs_getcwd" do
+    let(:pwd)    { '/current/directory' }
+    let(:output) { "#{pwd}\n" }
+
+    it "must run the 'pwd' command and return the path" do
+      expect(subject).to receive(:shell_exec).with('pwd').and_return(output)
+
+      expect(subject.fs_getcwd).to eq(pwd)
+    end
+  end
+
+  describe "#fs_chdir" do
+    let(:new_dir) { '/path/to/directory' }
+    let(:output)  { "\n" }
+
+    it "must run the 'cd <path> 2>/dev/null' command" do
+      expect(subject).to receive(:shell_puts).with("cd #{new_dir} 2>/dev/null")
+
+      subject.fs_chdir(new_dir)
+    end
+  end
+
+  describe "#fs_readfile" do
+    let(:path)     { '/path/to/file' }
+    let(:contents) { "foo bar\nbaz qux\n"  }
+
+    it "must run the 'cat <path>' command and return the file contents" do
+      expect(subject).to receive(:command_exec).with('cat',path).and_return(contents)
+
+      expect(subject.fs_readfile(path)).to eq(contents)
+    end
+  end
+
+  describe "#fs_readlink" do
+    let(:path)      { '/path/to/link'  }
+    let(:dest_path) { 'path/to/file'   }
+    let(:output)    { "#{dest_path}\n" }
+
+    it "must run the 'readlink -f <path>' command and return the link's path" do
+      expect(subject).to receive(:command_exec).with('readlink','-f',path).and_return(output)
+
+      expect(subject.fs_readlink(path)).to eq(dest_path)
+    end
+  end
+
+  describe "#fs_readdir" do
+    let(:path) { '/path/to/dir'  }
+    let(:entries) do
+      %w[
+        API_SPEC.md
+        ChangeLog.md
+        COPYING.txt
+        examples
+        Gemfile
+        Gemfile.lock
+        gemspec.yml
+        lib
+        Rakefile
+        README.md
+        ronin-post_ex.gemspec
+        spec
+        vendor
+      ]
+    end
+    let(:output) { "#{entries.join("\n")}\n" }
+
+    it "must run the 'ls <path>' command and return the directories entries" do
+      expect(subject).to receive(:command_exec).with('ls',path).and_return(output)
+
+      expect(subject.fs_readdir(path)).to eq(entries)
+    end
+  end
+
+  describe "#fs_glob" do
+    let(:pattern) { '*.md' }
+    let(:entries) do
+      %w[
+        API_SPEC.md
+        ChangeLog.md
+        README.md
+      ]
+    end
+    let(:output) { "#{entries.join("\n")}\n" }
+
+    it "must run the 'ls <pattern>' command and return the matching paths" do
+      expect(subject).to receive(:shell_exec).with("ls #{pattern}").and_return(output)
+
+      expect(subject.fs_glob(pattern)).to eq(entries)
+    end
+  end
+
+  describe "#fs_mktemp" do
+    let(:basename) { 'ronin-XXXX.txt' }
+    let(:tempfile) { 'ronin-LtFK.txt' }
+    let(:output) { "#{tempfile}\n" }
+
+    it "must run the 'mktemp <basename>' command and return the tempfile name" do
+      expect(subject).to receive(:command_exec).with('mktemp',basename).and_return(output)
+
+      expect(subject.fs_mktemp(basename)).to eq(tempfile)
+    end
+  end
+
+  describe "#fs_mkdir" do
+    let(:path) { '/path/to/new_dir' }
+
+    it "must run the 'mkdir <pattern>' command" do
+      expect(subject).to receive(:command_exec).with('mkdir',path)
+
+      subject.fs_mkdir(path)
+    end
+  end
+
+  describe "#fs_copy" do
+    let(:src)  { '/path/to/src'  }
+    let(:dest) { '/path/to/dest' }
+
+    it "must run the 'cp -r <src> <dest>' command" do
+      expect(subject).to receive(:command_exec).with('cp','-r',src,dest)
+
+      subject.fs_copy(src,dest)
+    end
+  end
+
+  describe "#fs_unlink" do
+    let(:path) { '/path/to/file' }
+
+    it "must run the 'rm <path>' command" do
+      expect(subject).to receive(:command_exec).with('rm',path)
+
+      subject.fs_unlink(path)
+    end
+  end
+
+  describe "#fs_rmdir" do
+    let(:path) { '/path/to/dir' }
+
+    it "must run the 'rmdir <path>' command" do
+      expect(subject).to receive(:command_exec).with('rmdir',path)
+
+      subject.fs_rmdir(path)
+    end
+  end
+
+  describe "#fs_move" do
+    let(:src)  { '/path/to/src'  }
+    let(:dest) { '/path/to/dest' }
+
+    it "must run the 'mv <src> <dest>' command" do
+      expect(subject).to receive(:command_exec).with('mv',src,dest)
+
+      subject.fs_move(src,dest)
+    end
+  end
+
+  describe "#fs_link" do
+    let(:src)  { '/path/to/src'  }
+    let(:dest) { '/path/to/dest' }
+
+    it "must run the 'ln -s <src> <dest>' command" do
+      expect(subject).to receive(:command_exec).with('ln','-s',src,dest)
+
+      subject.fs_link(src,dest)
+    end
+  end
+
+  describe "#fs_chgrp" do
+    let(:group) { 'wheel' }
+    let(:path)  { '/path/to/file' }
+
+    it "must run the 'chgrp <group> <path>' command" do
+      expect(subject).to receive(:command_exec).with('chgrp',group,path)
+
+      subject.fs_chgrp(group,path)
+    end
+  end
+
+  describe "#fs_chown" do
+    let(:user) { 'root' }
+    let(:path) { '/path/to/file' }
+
+    it "must run the 'chown <user> <path>' command" do
+      expect(subject).to receive(:command_exec).with('chown',user,path)
+
+      subject.fs_chown(user,path)
+    end
+  end
+
+  describe "#fs_chmod" do
+    let(:mode)  { 0777 }
+    let(:umask) { "%.4o" % mode }
+    let(:path)  { '/path/to/file' }
+
+    it "must run the 'chown <user> <path>' command" do
+      expect(subject).to receive(:command_exec).with('chmod',umask,path)
+
+      subject.fs_chmod(mode,path)
+    end
+  end
+
+  describe "#fs_stat" do
+    let(:path) { '/path/to/file' }
+    let(:size) { 420 }
+    let(:blocks) { 16 }
+    let(:uid) { 1000 }
+    let(:gid) { 1000 }
+    let(:inode) { 12345 }
+    let(:links) { 1 }
+    let(:atime) { Time.at(1668608914) }
+    let(:mtime) { Time.at(1668427627) }
+    let(:ctime) { Time.at(1668427627) }
+    let(:blocksize) { 1668427627 }
+
+    let(:output) do
+      "#{path} #{size} #{blocks} 81a4 #{uid} #{gid} fd01 #{inode} #{links} 0 0 #{atime.to_i} #{mtime.to_i} #{ctime.to_i} #{blocksize} 4096 unconfined_u:object_r:unlabeled_t:s0\n"
+    end
+
+    it "must run the 'stat -t <path>' command and return a Hash of parsed stat information" do
+      expect(subject).to receive(:command_exec).with('stat','-t',path).and_return(output)
+
+      expect(subject.fs_stat(path)).to eq(
+        {
+          path:      path,
+          size:      size,
+          blocks:    blocks,
+          uid:       uid,
+          gid:       gid,
+          inode:     inode,
+          links:     links,
+          atime:     atime,
+          mtime:     mtime,
+          ctime:     ctime,
+          blocksize: blocksize
+        }
+      )
+    end
+  end
+
+  describe "#process_getpid" do
+    let(:pid)    { 1234 }
+    let(:output) { "#{pid}\n" }
+
+    it "must run the 'echo $$' command and return the parsed PID" do
+      expect(subject).to receive(:shell_exec).with('echo $$').and_return(output)
+
+      expect(subject.process_getpid).to eq(pid)
+    end
+  end
+
+  describe "#process_getppid" do
+    let(:ppid)   { 1234 }
+    let(:output) { "#{ppid}\n" }
+
+    it "must run the 'echo $PPID' command and return the parsed PPID" do
+      expect(subject).to receive(:shell_exec).with('echo $PPID').and_return(output)
+
+      expect(subject.process_getppid).to eq(ppid)
+    end
+  end
+
+  describe "#process_getuid" do
+    let(:uid)    { 1000 }
+    let(:output) { "#{uid}\n" }
+
+    it "must run the 'id -u' command and return the parsed UID" do
+      expect(subject).to receive(:command_exec).with('id','-u').and_return(output)
+
+      expect(subject.process_getuid).to eq(uid)
+    end
+  end
+
+  describe "#process_getgid" do
+    let(:gid)    { 1000 }
+    let(:output) { "#{gid}\n" }
+
+    it "must run the 'id -g' command and return the parsed GID" do
+      expect(subject).to receive(:command_exec).with('id','-g').and_return(output)
+
+      expect(subject.process_getgid).to eq(gid)
+    end
+  end
+
+  describe "#process_getgid" do
+    let(:env) do
+      {
+        'FOO' => 'foo',
+        'BAR' => 'bar'
+      }
+    end
+    let(:output) do
+      "#{env.keys[0]}=#{env.values[0]}\n#{env.keys[1]}=#{env.values[1]}\n"
+    end
+
+    it "must run the 'env' command and return the parsed env Hash" do
+      expect(subject).to receive(:command_exec).with('env').and_return(output)
+
+      expect(subject.process_environ).to eq(env)
+    end
+  end
+
+  describe "#process_getenv" do
+    let(:name)  { 'FOO' }
+    let(:value) { 'foo bar baz' }
+    let(:output) { "#{value}\n" }
+
+    it "must run the 'echo $<name>' command and return the parsed value" do
+      expect(subject).to receive(:shell_exec).with("echo $#{name}").and_return(output)
+
+      expect(subject.process_getenv(name)).to eq(value)
+    end
+  end
+
+  describe "#process_setenv" do
+    let(:name)  { 'FOO' }
+    let(:value) { 'foo bar baz' }
+
+    it "must run the 'export <name>=<value>' command and return the parsed value" do
+      expect(subject).to receive(:shell_puts).with("export #{name}=#{value}")
+
+      subject.process_setenv(name,value)
+    end
+  end
+
+  describe "#process_unsetenv" do
+    let(:name) { 'FOO' }
+
+    it "must run the 'unset <name>' command" do
+      expect(subject).to receive(:shell_puts).with("unset #{name}")
+
+      subject.process_unsetenv(name)
+    end
+  end
+
+  describe "#process_kill" do
+    let(:pid)    { 1234   }
+    let(:signal) { 'TERM' }
+
+    it "must run the 'kill -s <signal> <pid>' command" do
+      expect(subject).to receive(:command_exec).with('kill','-s',signal,pid)
+
+      subject.process_kill(pid,signal)
+    end
+  end
+
+  describe "#process_spawn" do
+    let(:command_name) { 'cmd' }
+    let(:arguments)    { ['foo', 'bar baz'] }
+
+    let(:pid)    { 1234 }
+    let(:output) { "#{pid}\n" }
+
+    it "must run the command with arguments and return the parsed PID" do
+      command = Shellwords.join([command_name, *arguments])
+
+      expect(subject).to receive(:shell_exec).with("#{command} 2>&1 >/dev/null &; echo $!").and_return(output)
+
+      expect(subject.process_spawn(command_name,*arguments)).to eq(pid)
+    end
+  end
+
+  describe "#process_exit" do
+    it "must run the 'exit' command" do
+      expect(subject).to receive(:shell_puts).with('exit')
+
+      subject.process_exit
     end
   end
 
