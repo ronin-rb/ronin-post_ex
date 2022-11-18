542261c084508a6d5ff7799a9efeba456fcd571a
Postmodern <postmodern.mod3@gmail.com>
2022-11-18 11:23:33 -0800

    Add an error message if the example script cannot connect.

--- examples/bind_shell.rb
+++ examples/bind_shell.rb
@@ -6,7 +6,12 @@ require 'ronin/post_ex/system'
 require 'socket'
 
 # run `nc -l -p 1337 -e /bin/sh` in another terminal
-socket = TCPSocket.new('localhost',1337)
+socket = begin
+           TCPSocket.new('localhost',1337)
+         rescue
+           warn "Please run 'nc -l -p 1337 -e /bin/sh' in another terminal"
+           exit(-1)
+         end
 
 session = Ronin::PostEx::Sessions::ShellSession.new(socket)
 system  = Ronin::PostEx::System.new(session)
