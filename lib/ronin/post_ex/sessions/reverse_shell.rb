15c63d09a8cc0a56398c6946896d5808e455fda5
Postmodern <postmodern.mod3@gmail.com>
2022-12-27 16:22:46 -0800

    Added `Ronin::PostEx::Sessions::ReverseShell` (closes #41).

---
+++ lib/ronin/post_ex/sessions/reverse_shell.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+#
+# ronin-post_ex - a Ruby API for Post-Exploitation.
+#
+# Copyright (c) 2007-2022 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# ronin-post_ex is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-post_ex is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-post_ex.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+require 'ronin/post_ex/sessions/shell_session'
+
+require 'socket'
+
+module Ronin
+  module PostEx
+    module Sessions
+      class ReverseShell < ShellSession
+
+        #
+        # Listens for and accepts a reverse shell connection.
+        #
+        # @param [String, nil] host
+        #   The host to connect to.
+        #
+        # @param [Integer] port
+        #   The port to connect to.
+        #
+        # @return [ReverseShell]
+        #   The new reverse shell session.
+        #
+        def self.listen(host=nil,port)
+          server_socket = TCPServer.new(port,host)
+          server_socket.listen(1)
+
+          new(server_socket.accept)
+        ensure
+          server_socket.close
+        end
+
+      end
+    end
+  end
+end
---
+++ spec/sessions/reverse_shell_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+require 'ronin/post_ex/sessions/reverse_shell'
+
+describe Ronin::PostEx::Sessions::ReverseShell do
+  it "must inherit from Ronin::PostEx::Sessions::ShellSession" do
+    expect(described_class).to be < Ronin::PostEx::Sessions::ShellSession
+  end
+
+  describe ".listen" do
+    let(:port) { 1337 }
+
+    let(:server_socket) { double('TCPServer') }
+    let(:client_socket) { double('TCPSocket') }
+
+    subject { described_class }
+
+    it "must listen on a local port, accept a connection, return a #{described_class} object, and close the server socket" do
+      expect(TCPServer).to receive(:new).with(port,nil).and_return(server_socket)
+      expect(server_socket).to receive(:listen).with(1)
+      expect(server_socket).to receive(:accept).and_return(client_socket)
+      expect(server_socket).to receive(:close)
+
+      reverse_shell = subject.listen(port)
+
+      expect(reverse_shell).to be_kind_of(described_class)
+      expect(reverse_shell.io).to be(client_socket)
+    end
+
+    context "and a host argument is given" do
+      let(:host) { '127.0.0.1' }
+
+      it "must listen on a local host and port, accept a connection, return a #{described_class} object, and close the server socket" do
+        expect(TCPServer).to receive(:new).with(port,host).and_return(server_socket)
+        expect(server_socket).to receive(:listen).with(1)
+        expect(server_socket).to receive(:accept).and_return(client_socket)
+        expect(server_socket).to receive(:close)
+
+        reverse_shell = subject.listen(host,port)
+
+        expect(reverse_shell).to be_kind_of(described_class)
+        expect(reverse_shell.io).to be(client_socket)
+      end
+    end
+  end
+end
